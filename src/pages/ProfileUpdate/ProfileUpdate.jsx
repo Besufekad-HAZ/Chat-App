040038eaeb1f67590c1e51ea10dd5c34c0245fd8
Besufekad-HAZ <besurye@gmail.com>
2024-12-16 23:15:27 +0300

    Added profile update form with fields for uploading profile image, name, bio, and save button.

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,7 +1,25 @@
 import "./ProfileUpdate.css";
+import assets from "../../assets/assets";
 
 const ProfileUpdate = () => {
-  return <div>Your are on the Profile Page</div>;
+  return (
+    <div className="profile">
+      <div className="profile-container">
+        <form action="">
+          <h3>Profile Details</h3>
+          <label htmlFor="name">
+            <input type="file" id="avatar" accept="image/*" hidden />
+            <img src={assets.avatar_icon} alt="" />
+            Upload profile image
+          </label>
+          <input type="text" id="name" placeholder="Your name" required />
+          <textarea placeholder="Write profile bio" required></textarea>
+          <button type="submit">Save</button>
+        </form>
+        <img src={assets.logo_icon} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default ProfileUpdate;
