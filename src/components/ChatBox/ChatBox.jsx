ffc48cb665fe6474710fe652ce61cd4f5aae11ac
Besufekad-HAZ <besurye@gmail.com>
2024-12-16 19:56:25 +0300

    Styled chat box and chat user section with height, padding, flex layout, and background color.

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -1,14 +1,18 @@
 import "./ChatBox.css";
-import assets from "../../assets"; // Adjust the path as necessary
+import assets from "../../assets/assets"; // Adjust the path as necessary
 
 const ChatBox = () => {
-  return <div className="chat-box">
-    <div className="chat-user">
-      <img src={assets.profile_img} alt="" />
-      <p>Richard Sanford <img src={assets.green_dot} alt="" /></p>
-      <img src={assets.help_icon} className="help" alt="" />
+  return (
+    <div className="chat-box">
+      <div className="chat-user">
+        <img src={assets.profile_img} alt="" />
+        <p>
+          Richard Sanford <img src={assets.green_dot} alt="" />
+        </p>
+        <img src={assets.help_icon} className="help" alt="" />
+      </div>
     </div>
-    </div>;
+  );
 };
 
 export default ChatBox;
