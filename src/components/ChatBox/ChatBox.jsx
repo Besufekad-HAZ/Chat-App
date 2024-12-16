39dd8f73c01e6b31c173a042fae0b2da15d3f44a
Besufekad-HAZ <besurye@gmail.com>
2024-12-16 20:08:34 +0300

    Added chat input section with text input, file input for images, and send button.

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -7,9 +7,17 @@ const ChatBox = () => {
       <div className="chat-user">
         <img src={assets.profile_img} alt="" />
         <p>
-          Richard Sanford <img src={assets.green_dot} alt="" />
+          Richard Sanford <img className="dot" src={assets.green_dot} alt="" />
         </p>
         <img src={assets.help_icon} className="help" alt="" />
+      </div>
+      <div className="chat-input">
+        <input type="text" placeholder="Type a message" />
+        <input type="file" id="image" accept="image/*" hidden />
+        <label htmlFor="image">
+          <img src={assets.gallery_icon} alt="" />
+        </label>
+        <img src={assets.send_button} alt="" />
       </div>
     </div>
   );
