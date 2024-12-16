74717b56392c211fe428a18676d66fde3424fee4
Besufekad-HAZ <besurye@gmail.com>
2024-12-16 16:45:56 +0300

    Work on the LeftSidebar of the Chat page section

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -1,7 +1,32 @@
 import "./LeftSidebar.css";
+import assets from "../../assets/assets";
 
 const LeftSidebar = () => {
-  return <div>LeftSidebar</div>;
+  return (
+    <div className="ls">
+      <div className="ls-top">
+        <div className="ls-nav">
+          <img src={assets.logo} alt="nav logo" className="logo" />
+          <div className="menu">
+            <img src={assets.menu_icon} alt="" />
+          </div>
+        </div>
+        <div className="ls-search">
+          <img src={assets.search_icon} alt="" />
+          <input type="text" placeholder="Search here.." />
+        </div>
+      </div>
+      <div className="ls-list">
+        <div className="friends">
+          <img src={assets.profile_img} alt="" />
+          <div>
+            <p> Richard Sanford</p>
+            <span>Hello, how are you?</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LeftSidebar;
