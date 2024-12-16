4352c848d4e79c75c22a8742c67947324dc7d9c7
Besufekad-HAZ <besurye@gmail.com>
2024-12-16 19:51:37 +0300

    Added user profile image, name with status indicator, and help icon to the chat user section.

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -1,6 +1,14 @@
 import "./ChatBox.css";
+import assets from "../../assets"; // Adjust the path as necessary
+
 const ChatBox = () => {
-  return <div>ChatBox</div>;
+  return <div className="chat-box">
+    <div className="chat-user">
+      <img src={assets.profile_img} alt="" />
+      <p>Richard Sanford <img src={assets.green_dot} alt="" /></p>
+      <img src={assets.help_icon} className="help" alt="" />
+    </div>
+    </div>;
 };
 
 export default ChatBox;
