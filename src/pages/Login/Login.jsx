dc4d41626466d37270e1e6ed09c3abd0f1d86310
Besufekad-HAZ <besurye@gmail.com>
2024-12-16 15:48:09 +0300

    Create the login page layout using html in the react component

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,42 @@
 import "./Login.css";
+import assets from "../../assets/assets";
 const Login = () => {
-  return <div> You are on the Login Page</div>;
+  return (
+    <div className="login">
+      <img src={assets.logo_big} alt="Login logo image" className="logo" />
+      <form className="login-form">
+        <h2>Sign Up</h2>
+        <input
+          type="text"
+          placeholder="username"
+          className="form-input"
+          required
+        />
+        <input
+          type="email"
+          placeholder="Email Address"
+          className="form-input"
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="form-input"
+          required
+        />
+        <button type="submit">Sign Up</button>
+        <div className="login-term">
+          <input type="checkbox" />
+          <p>Accept terms of use and privacy policy</p>
+        </div>
+        <div className="login-forgot">
+          <p className="login-toogle">
+            Already have an account <span>click here</span>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
