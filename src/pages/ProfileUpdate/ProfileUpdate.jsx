1f2f8caa726966f055fdef62e27df74b6570d64a
Besufekad-HAZ <besurye@gmail.com>
2025-01-16 14:27:10 +0300

    Fix issue with bio being applied to the name input field in ProfileUpdate component

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,11 +1,40 @@
 import "./ProfileUpdate.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import assets from "../../assets/assets";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth, db } from "../../config/firebase";
+import { doc, getDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
 const ProfileUpdate = () => {
+  const navigate = useNavigate();
   const [image, setImage] = useState(false);
   const [name, setName] = useState("");
   const [bio, setBio] = useState("");
+  const [uid, setUid] = useState("");
+  const [prevImage, setPrevImage] = useState("");
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        setUid(user.uid);
+        const docRef = doc(db, "users", user.uid);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.data().name) {
+          setName(docSnap.data().name);
+        }
+        if (docSnap.data().bio) {
+          setBio(docSnap.data().bio);
+        }
+        if (docSnap.data().avatar) {
+          setPrevImage(docSnap.data().avatar);
+        }
+      } else {
+        // User is signed out
+        navigate("/");
+      }
+    });
+  }, []);
 
   return (
     <div className="profile">
@@ -35,7 +64,7 @@ const ProfileUpdate = () => {
             required
           />
           <textarea
-            onChange={(e) => setBio(e.target.value)}
+            onChange={(e) => setBio(e.target.bio)}
             value={bio}
             placeholder="Write profile bio"
             required
