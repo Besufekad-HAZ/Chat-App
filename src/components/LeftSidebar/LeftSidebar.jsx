c0b6c13afc21b6170e13efa83e95ce592127604a
Besufekad-HAZ <besurye@gmail.com>
2025-01-17 11:56:00 +0300

    Update inputHandler to manage showSearch state and set user state based on query results.

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -3,20 +3,30 @@ import assets from "../../assets/assets";
 import { useNavigate } from "react-router-dom";
 import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "../../config/firebase";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "../../context/AppContext";
 
 const LeftSidebar = () => {
   const navigate = useNavigate();
   const { userData } = useContext(AppContext);
+  const [user, setUser] = useState(null);
+  const [showSearch, setshowSearch] = useState(false);
+
   const inputHandler = async (e) => {
     try {
       const input = e.target.value;
-      const userRef = collection(db, "users");
-      const q = query(userRef, where("username", "==", input.toLowerCase()));
-      const querySnap = await getDocs(q);
-      if (!querySnap.empty && querySnap.docs[0].data().id !== userData.id) {
-        console.log(querySnap.docs[0].data());
+      if (input) {
+        setshowSearch(true);
+        const userRef = collection(db, "users");
+        const q = query(userRef, where("username", "==", input.toLowerCase()));
+        const querySnap = await getDocs(q);
+        if (!querySnap.empty && querySnap.docs[0].data().id !== userData.id) {
+          setUser(querySnap.docs[0].data());
+        } else {
+          setUser(null);
+        }
+      } else {
+        setshowSearch(false);
       }
     } catch (err) {
       console.error(err);
